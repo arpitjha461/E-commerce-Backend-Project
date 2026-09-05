5c5462a101ac525830fe9f6dfbae1d6ba6e7817a
ARPIT VISHWAKARMA <arpitjha461@gmail.com>
2026-09-05 18:40:25 +0530

    update payment module

--- ecommerce/src/main/java/com/arpit/ecommerce/exception/GlobalExceptionHandler.java
+++ ecommerce/src/main/java/com/arpit/ecommerce/exception/GlobalExceptionHandler.java
@@ -90,9 +90,12 @@ public class GlobalExceptionHandler {
         }
 
         @ExceptionHandler(CartEmptyException.class)
-        public ResponseEntity<String> handleCartEmptyException(CartEmptyException ex){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(ex.getMessage());
+        public ResponseEntity<ApiError> handleCartEmptyException(CartEmptyException ex){
+            ApiError apiError = new ApiError();
+            apiError.setTimestamp(LocalDateTime.now());
+            apiError.setMessage(ex.getMessage());
+            apiError.setStatus(HttpStatus.BAD_REQUEST.value());
+            return new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
         }
         @ExceptionHandler(OrderNotFoundException.class)
         public ResponseEntity<ApiError> handleOrderNotFoundException(OrderNotFoundException ex){
@@ -131,6 +134,29 @@ public class GlobalExceptionHandler {
         apiError.setStatus(HttpStatus.BAD_REQUEST.value());
 
         return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(PaymentAlreadyExistsException.class)
+    public ResponseEntity<ApiError> handlePaymentAlreadyExistsException(
+            PaymentAlreadyExistsException ex) {
+
+        ApiError apiError = new ApiError();
+        apiError.setTimestamp(LocalDateTime.now());
+        apiError.setMessage(ex.getMessage());
+        apiError.setStatus(HttpStatus.CONFLICT.value());
+
+        return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UnauthorizedPaymentException.class)
+    public ResponseEntity<ApiError> handleUnauthorizedPaymentException(
+            UnauthorizedPaymentException ex) {
+
+        ApiError apiError = new ApiError();
+        apiError.setTimestamp(LocalDateTime.now());
+        apiError.setMessage(ex.getMessage());
+        apiError.setStatus(HttpStatus.FORBIDDEN.value());
+
+        return new ResponseEntity<>(apiError, HttpStatus.FORBIDDEN);
     }
 }
 
