40b63f2cc53da81fb248e1570c9ecf78d33d9a70
ARPIT VISHWAKARMA <arpitjha461@gmail.com>
2026-09-10 18:13:35 +0530

    Implement Inventory requestdto, repo, exceptions

---
+++ ecommerce/src/main/java/com/arpit/ecommerce/exception/InsufficientStockException.java
@@ -0,0 +1,7 @@
+package com.arpit.ecommerce.exception;
+
+public class InsufficientStockException extends RuntimeException{
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
